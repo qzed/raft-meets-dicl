8fde6f5218a409857d477d1c07ad5ec638cbf98d
Maximilian Luz <luzmaximilian@gmail.com>
2021-07-09 04:35:24 +0200

    Add sanity wrapper for OpenCV imshow and waitKey

---
+++ src/visual/imshow.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+class ImageWindow:
+    def __init__(self, title):
+        self.title = title
+
+    def wait(self):
+        # Using OpenCV imshow with waitKey(0) can deadlock if the window is
+        # closed by pressing the 'x' button: It still waits for a key, but it
+        # can't get any input anymore due to the window being gone. This
+        # wouldn't be too annoying if OpenCV didn't also block any attemtps to
+        # Ctrl-C waitKey(0). Work around this by repeatedly checking if the
+        # window is still there. This makes it behave nicely to both closing
+        # the window and Ctrl-C.
+
+        while cv2.getWindowProperty(self.title, cv2.WND_PROP_VISIBLE) >= 1:
+            if cv2.waitKey(250) != -1:
+                break
+
+
+def imshow(title, rgb):
+    cv2.imshow(title, rgb[:, :, ::-1])
+    return ImageWindow(title)
