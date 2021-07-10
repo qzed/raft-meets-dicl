05b410d09a73e830bfb727256465fcd1934520aa
Maximilian Luz <luzmaximilian@gmail.com>
2021-07-10 23:52:13 +0200

    Add flip augmentation

--- src/data/augment.py
+++ src/data/augment.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from . import config
 from .collection import Collection
 
@@ -39,8 +41,49 @@ class Augmentation:
         raise NotImplementedError
 
 
+class Flip(Augmentation):
+    @classmethod
+    def from_config(cls, cfg):
+        if cfg['type'] != 'flip':
+            raise ValueError(f"invalid augmentation type '{cfg['type']}', expected 'flip'")
+
+        prob = list(cfg['probability'])
+        if len(prob) != 2:
+            raise ValueError('invalid flip probability, expected list or tuple with two elements')
+
+        return cls(prob)
+
+    def __init__(self, probability):
+        self.probability = probability
+
+    def get_config(self):
+        return {
+            'type': 'flip',
+            'size': self.probability,
+        }
+
+    def process(self, img1, img2, flow, valid):
+        # horizontal flip
+        if np.random.rand() < self.probability[0]:
+            img1 = img1[:, ::-1]
+            img2 = img2[:, ::-1]
+            flow = flow[:, ::-1] * (-1.0, 1.0)
+            valid = valid[:, ::-1]
+
+        # vertical flip
+        if np.random.rand() < self.probability[1]:
+            img1 = img1[::-1, :]
+            img2 = img2[::-1, :]
+            flow = flow[::-1, :] * (1.0, -1.0)
+            valid = valid[::-1, :]
+
+        return img1, img2, flow, valid
+
+
 def _build_augmentation(cfg):
-    types = {}
+    types = {
+        'flip': Flip.from_config,
+    }
 
     ty = cfg['type']
     if ty not in types.keys():
