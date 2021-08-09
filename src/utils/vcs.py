e38ed8081be2bc317244c8c2dbdd393348728969
Maximilian Luz <luzmaximilian@gmail.com>
2021-08-10 00:58:39 +0200

    Add prefix to git hash if repo is dirty

--- src/utils/vcs.py
+++ src/utils/vcs.py
@@ -3,9 +3,14 @@ from pathlib import Path
 import git
 
 
-def get_git_head_hash(default=None):
+def get_git_head_hash(default=None, pfx_dirty='~'):
     try:
         repo = git.Repo(Path(__file__).parent, search_parent_directories=True)
-        return repo.head.object.hexsha
+
+        if repo.is_dirty():
+            return pfx_dirty + repo.head.object.hexsha
+        else:
+            return repo.head.object.hexsha
+
     except git.exc.InvalidGitRepositoryError:
         return default
