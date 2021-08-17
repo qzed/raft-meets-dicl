0451f26d6a60465f73200427d3f76d89fa1acf45
Maximilian Luz <luzmaximilian@gmail.com>
2021-08-17 17:39:42 +0200

    Set seeds for CUDA backend to ensure reproducibility

--- src/utils/seeds.py
+++ src/utils/seeds.py
@@ -13,20 +13,23 @@ class Seeds:
     python: int
     numpy: int
     torch: int
+    cuda: int
 
     def get_config(self):
         return {
             'python': self.python,
             'numpy': self.numpy,
             'torch': self.torch,
+            'cuda': self.cuda,
         }
 
     def apply(self):
-        logging.info(f"seeding: python={self.python}, numpy={self.numpy}, torch={self.torch}")
+        logging.info(f"seeding: python={self.python}, numpy={self.numpy}, torch={self.torch}, cuda={self.cuda}")
 
         random.seed(self.python)
         np.random.seed(self.numpy)
         torch.manual_seed(self.torch)
+        torch.cuda.manual_seed_all(self.cuda)
         return self
 
 
@@ -34,8 +37,9 @@ def from_config(cfg):
     p = cfg['python']
     n = cfg['numpy']
     t = cfg['torch']
+    c = cfg['cuda']
 
-    return Seeds(python=p, numpy=n, torch=t)
+    return Seeds(python=p, numpy=n, torch=t, cuda=c)
 
 
 def _urandom_i64():
@@ -50,6 +54,6 @@ def _urandom_u32():
 
 def random_seeds():
     # get some hopefully good seeds from the OS
-    p, n, t = _urandom_i64(), _urandom_u32(), _urandom_i64()
+    p, n, t, c = _urandom_i64(), _urandom_u32(), _urandom_i64(), _urandom_i64()
 
-    return Seeds(python=p, numpy=n, torch=t)
+    return Seeds(python=p, numpy=n, torch=t, cuda=c)
