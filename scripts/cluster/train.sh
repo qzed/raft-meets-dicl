2f2452e40e07b50bdd2bd68e127d4597520a61cf
Maximilian Luz <luzmaximilian@gmail.com>
2021-11-14 00:20:56 +0100

    scripts: Add example usage to cluster training script

--- scripts/cluster/train.sh
+++ scripts/cluster/train.sh
@@ -4,6 +4,12 @@
 #SBATCH --mem=32gb
 #SBATCH --gres=gpu:2
 
+# Example usage:
+#
+# sbatch -p dev_gpu_4 --time=00:15:00 ./scripts/cluster/train.sh    \
+#        --config cfg/full/dev/dicl-baseline.flyingchairs2.json     \
+#        --reproduce --suffix testing --comment "Some test run"
+
 echo "============================== SETTING UP =============================="
 echo ""
 
