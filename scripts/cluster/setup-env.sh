8a8cfbb753177831cb642041933352e9658441d0
Maximilian Luz <luzmaximilian@gmail.com>
2021-11-13 18:21:48 +0100

    scripts: Add script for setting up cluster environment

---
+++ scripts/cluster/setup-env.sh
@@ -0,0 +1,6 @@
+# DO NOT EXECUTE, NEEDS TO BE SOURCED TO HAVE ANY EFFECT
+export PIPENV_VENV_IN_PROJECT="enabled"
+
+module load devel/python/3.9.5_gnu_11.1
+module load devel/cuda/10.2
+module load devel/cudnn/10.2
