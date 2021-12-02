263a7b5503ae756df3684958725cd698e4ada544
Maximilian Luz <luzmaximilian@gmail.com>
2021-12-02 16:33:51 +0100

    models/raft: Make context and recurrent channels configurable

--- src/models/impls/raft.py
+++ src/models/impls/raft.py
@@ -333,13 +333,14 @@ class RaftModule(nn.Module):
     """RAFT flow estimation network"""
 
     def __init__(self, dropout=0.0, mixed_precision=False, upnet=True, corr_levels=4, corr_radius=4,
-                 corr_channels=256, encoder_norm='instance', context_norm='batch'):
+                 corr_channels=256, context_channels=128, recurrent_channels=128,
+                 encoder_norm='instance', context_norm='batch'):
         super().__init__()
 
         self.mixed_precision = mixed_precision
 
-        self.hidden_dim = hdim = 128
-        self.context_dim = cdim = 128
+        self.hidden_dim = hdim = recurrent_channels
+        self.context_dim = cdim = context_channels
 
         self.corr_levels = corr_levels
         self.corr_radius = corr_radius
@@ -442,6 +443,8 @@ class Raft(Model):
         corr_levels = param_cfg.get('corr-levels', 4)
         corr_radius = param_cfg.get('corr-radius', 4)
         corr_channels = param_cfg.get('corr-channels', 256)
+        context_channels = param_cfg.get('context-channels', 128)
+        recurrent_channels = param_cfg.get('recurrent-channels', 128)
         encoder_norm = param_cfg.get('encoder-norm', 'instance')
         context_norm = param_cfg.get('context-norm', 'batch')
 
@@ -449,22 +452,27 @@ class Raft(Model):
 
         return cls(dropout=dropout, mixed_precision=mixed_precision, upnet=upnet,
                    corr_levels=corr_levels, corr_radius=corr_radius, corr_channels=corr_channels,
+                   context_channels=context_channels, recurrent_channels=recurrent_channels,
                    encoder_norm=encoder_norm, context_norm=context_norm, arguments=args)
 
     def __init__(self, dropout=0.0, mixed_precision=False, upnet=True, corr_levels=4, corr_radius=4,
-                 corr_channels=256, encoder_norm='instance', context_norm='batch', arguments={}):
+                 corr_channels=256, context_channels=128, recurrent_channels=128,
+                 encoder_norm='instance', context_norm='batch', arguments={}):
         self.dropout = dropout
         self.mixed_precision = mixed_precision
         self.upnet = upnet
         self.corr_levels = corr_levels
         self.corr_radius = corr_radius
         self.corr_channels = corr_channels
+        self.context_channels = context_channels
+        self.recurrent_channels = recurrent_channels
         self.encoder_norm = encoder_norm
         self.context_norm = context_norm
 
         super().__init__(RaftModule(dropout=dropout, mixed_precision=mixed_precision, upnet=upnet,
                                     corr_levels=corr_levels, corr_radius=corr_radius,
-                                    corr_channels=corr_channels, encoder_norm=encoder_norm,
+                                    corr_channels=corr_channels, context_channels=context_channels,
+                                    recurrent_channels=recurrent_channels, encoder_norm=encoder_norm,
                                     context_norm=context_norm), arguments)
 
         self.adapter = RaftAdapter()
@@ -480,6 +488,8 @@ class Raft(Model):
                 'corr-levels': self.corr_levels,
                 'corr-radius': self.corr_radius,
                 'corr-channels': self.corr_channels,
+                'context-channels': self.context_channels,
+                'recurrent-channels': self.recurrent_channels,
                 'upnet': self.upnet,
                 'encoder-norm': self.encoder_norm,
                 'context-norm': self.context_norm,
