300039535dce5dba3eb618b959bb4abcf2e9b9b7
Maximilian Luz <luzmaximilian@gmail.com>
2021-12-12 18:19:01 +0100

    models/raft+dicl-ctf2: Add RAFT pooling-based feature encoders

--- src/models/impls/raft_dicl_ctf_l2.py
+++ src/models/impls/raft_dicl_ctf_l2.py
@@ -147,6 +147,81 @@ class RaftFeatureEncoder(nn.Module):
         return x3, x4
 
 
+# -- RAFT-based pooling feature encoder ------------------------------------------------------------
+
+class RaftPoolFeatureEncoder(nn.Module):
+    """RAFT-based feature / context encoder network using pooling for coarser layer"""
+
+    def __init__(self, output_dim=128, norm_type='batch', dropout=0.0, pool_type='avg'):
+        super().__init__()
+
+        # input convolution             # (H, W, 3) -> (H/2, W/2, 64)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        self.norm1 = _make_norm2d(norm_type, num_channels=64, num_groups=8)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        # residual blocks
+        self.layer1 = nn.Sequential(    # (H/2, W/2, 64) -> (H/2, W/2, 64)
+            ResidualBlock(64, 64, norm_type, stride=1),
+            ResidualBlock(64, 64, norm_type, stride=1),
+        )
+
+        self.layer2 = nn.Sequential(    # (H/2, W/2, 64) -> (H/4, W/4, 96)
+            ResidualBlock(64, 96, norm_type, stride=2),
+            ResidualBlock(96, 96, norm_type, stride=1),
+        )
+
+        self.layer3 = nn.Sequential(    # (H/4, W/4, 96) -> (H/8, W/8, 128)
+            ResidualBlock(96, 128, norm_type, stride=2),
+            ResidualBlock(128, 128, norm_type, stride=1),
+        )
+
+        # output convolution            # (H/8, W/8, 128) -> (H/8, W/8, output_dim)
+        self.conv2 = nn.Conv2d(128, output_dim, kernel_size=1)
+
+        # dropout
+        self.dropout3 = nn.Dropout2d(p=dropout)
+        self.dropout4 = nn.Dropout2d(p=dropout)
+
+        # pooling
+        if pool_type == 'avg':
+            self.pool4 = nn.AvgPool2d(kernel_size=2, stride=2)
+        elif pool_type == 'max':
+            self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        else:
+            raise ValueError(f"invalid pool_type value: '{pool_type}'")
+
+        # initialize weights
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
+                if m.weight is not None:
+                    nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        # input layer
+        x = self.relu1(self.norm1(self.conv1(x)))
+
+        # residual blocks
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        # output layer
+        x = self.conv2(x)
+
+        # pooling
+        x3 = self.dropout3(x)
+
+        x = self.pool4(x)
+        x4 = self.dropout4(x)
+
+        return x3, x4
+
+
 # -- DICL-based feature encoder --------------------------------------------------------------------
 
 class ConvBlock(nn.Sequential):
@@ -864,6 +939,10 @@ class RaftPlusDiclModule(nn.Module):
 
         if encoder_type == 'raft':
             self.fnet = RaftFeatureEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=0)
+        elif context_type == 'raft-avgpool':
+            self.fnet = RaftPoolFeatureEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=0, pool_type='avg')
+        elif context_type == 'raft-maxpool':
+            self.fnet = RaftPoolFeatureEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=0, pool_type='max')
         elif encoder_type == 'dicl':
             self.fnet = DiclFeatureEncoder(output_dim=corr_channels, norm_type=encoder_norm)
         elif encoder_type == 'rfpm-raft':
@@ -873,6 +952,10 @@ class RaftPlusDiclModule(nn.Module):
 
         if context_type == 'raft':
             self.cnet = RaftFeatureEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=0)
+        elif context_type == 'raft-avgpool':
+            self.cnet = RaftPoolFeatureEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=0, pool_type='avg')
+        elif context_type == 'raft-maxpool':
+            self.cnet = RaftPoolFeatureEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=0, pool_type='max')
         elif context_type == 'dicl':
             self.cnet = DiclFeatureEncoder(output_dim=hdim+cdim, norm_type=context_norm)
         elif context_type == 'rfpm-raft':
