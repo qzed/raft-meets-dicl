535e5d0b88c13b13691a912f0a54c5b23b929703
Maximilian Luz <luzmaximilian@gmail.com>
2021-12-13 04:31:32 +0100

    models: Remove more duplicate encoder code

--- src/models/impls/raft_fs.py
+++ src/models/impls/raft_fs.py
@@ -17,73 +17,6 @@ from .. import common
 from . import raft
 
 
-class BasicEncoder(nn.Module):
-    """Feature / context encoder network"""
-
-    def __init__(self, output_dim=128, norm_type='batch', dropout=0.0):
-        super().__init__()
-
-        # input convolution             # (H, W, 3) -> (H/2, W/2, 64)
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
-        self.norm1 = common.norm.make_norm2d(norm_type, num_channels=64, num_groups=8)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        # residual blocks
-        self.layer1 = nn.Sequential(    # (H/2, W/2, 64) -> (H/2, W/2, 64)
-            raft.ResidualBlock(64, 64, norm_type, stride=1),
-            raft.ResidualBlock(64, 64, norm_type, stride=1),
-        )
-
-        self.layer2 = nn.Sequential(    # (H/2, W/2, 64) -> (H/4, W/4, 96)
-            raft.ResidualBlock(64, 96, norm_type, stride=2),
-            raft.ResidualBlock(96, 96, norm_type, stride=1),
-        )
-
-        self.layer3 = nn.Sequential(    # (H/4, W/4, 96) -> (H/8, W/8, 128)
-            raft.ResidualBlock(96, 128, norm_type, stride=2),
-            raft.ResidualBlock(128, 128, norm_type, stride=1),
-        )
-
-        # output convolution            # (H/8, W/8, 128) -> (H/8, W/8, output_dim)
-        self.conv2 = nn.Conv2d(128, output_dim, kernel_size=1)
-
-        # dropout
-        self.dropout = nn.Dropout2d(p=dropout)
-
-        # initialize weights
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.InstanceNorm2d, nn.GroupNorm)):
-                if m.weight is not None:
-                    nn.init.constant_(m.weight, 1)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        # input may be tuple/list for flow network (img1, img2), combine this into single batch
-        is_list = isinstance(x, tuple) or isinstance(x, list)
-        if is_list:
-            batch_dim = x[0].shape[0]
-            x = torch.cat(x, dim=0)
-
-        # input layer
-        x = self.relu1(self.norm1(self.conv1(x)))
-
-        # residual blocks
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-
-        # output layer
-        x = self.dropout(self.conv2(x))
-
-        if is_list:
-            x = torch.split(x, (batch_dim, batch_dim), dim=0)
-
-        return x
-
-
 class CorrBlock:
     """Correlation volume for matching costs"""
 
@@ -178,8 +111,8 @@ class RaftModule(nn.Module):
         self.corr_radius = corr_radius
         corr_planes = self.corr_levels * (2 * self.corr_radius + 1)**2
 
-        self.fnet = BasicEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=dropout)
-        self.cnet = BasicEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=dropout)
+        self.fnet = raft.BasicEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=dropout)
+        self.cnet = raft.BasicEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=dropout)
 
         self.update_block = raft.BasicUpdateBlock(corr_planes, input_dim=cdim, hidden_dim=hdim)
         self.upnet = raft.Up8Network(hdim)
