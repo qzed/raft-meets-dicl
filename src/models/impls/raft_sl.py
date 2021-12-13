0be0cd6ab48c0497176417b39fe71230dbf4d3d4
Maximilian Luz <luzmaximilian@gmail.com>
2021-12-13 05:09:06 +0100

    Avoid more code duplication

--- src/models/impls/raft_sl.py
+++ src/models/impls/raft_sl.py
@@ -1,96 +1,5 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 from .. import Model, ModelAdapter
-from .. import common
-
 from . import raft
-
-
-class RaftModule(nn.Module):
-    """RAFT flow estimation network"""
-
-    def __init__(self, dropout=0.0, mixed_precision=False, corr_radius=4,
-                 corr_channels=256, context_channels=128, recurrent_channels=128,
-                 encoder_norm='instance', context_norm='batch'):
-        super().__init__()
-
-        self.mixed_precision = mixed_precision
-
-        self.hidden_dim = hdim = recurrent_channels
-        self.context_dim = cdim = context_channels
-
-        self.corr_radius = corr_radius
-        corr_planes = (2 * self.corr_radius + 1)**2
-
-        self.fnet = raft.BasicEncoder(output_dim=corr_channels, norm_type=encoder_norm, dropout=dropout)
-        self.cnet = raft.BasicEncoder(output_dim=hdim+cdim, norm_type=context_norm, dropout=dropout)
-
-        self.update_block = raft.BasicUpdateBlock(corr_planes, input_dim=cdim, hidden_dim=hdim)
-        self.upnet = raft.Up8Network(hdim)
-
-    def freeze_batchnorm(self):
-        for m in self.modules():
-            if isinstance(m, nn.BatchNorm2d):
-                m.eval()
-
-    def initialize_flow(self, img):
-        # flow is represented as difference between two coordinate grids (flow = coords1 - coords0)
-        batch, _c, h, w = img.shape
-
-        coords = common.grid.coordinate_grid(batch, h // 8, w // 8, device=img.device)
-        return coords, coords.clone()
-
-    def forward(self, img1, img2, iterations=12, flow_init=None, upnet=True):
-        hdim, cdim = self.hidden_dim, self.context_dim
-
-        # run feature network
-        with torch.cuda.amp.autocast(enabled=self.mixed_precision):
-            fmap1, fmap2 = self.fnet((img1, img2))
-
-        fmap1, fmap2 = fmap1.float(), fmap2.float()
-
-        # build correlation volume
-        corr_vol = raft.CorrBlock(fmap1, fmap2, num_levels=1, radius=self.corr_radius)
-
-        # run context network
-        with torch.cuda.amp.autocast(enabled=self.mixed_precision):
-            h, x = torch.split(self.cnet(img1), (hdim, cdim), dim=1)
-            h, x = torch.tanh(h), torch.relu(x)
-
-        # initialize flow
-        coords0, coords1 = self.initialize_flow(img1)
-        if flow_init is not None:
-            coords1 += flow_init
-
-        flow = coords1 - coords0
-
-        # iteratively predict flow
-        out = []
-        for _ in range(iterations):
-            coords1 = coords1.detach()
-
-            # indes correlation volume
-            corr = corr_vol(coords1)
-
-            # estimate delta for flow update
-            with torch.cuda.amp.autocast(enabled=self.mixed_precision):
-                h, d = self.update_block(h, x, corr, flow)
-
-            # update flow estimate
-            coords1 = coords1 + d
-            flow = coords1 - coords0
-
-            # upsample flow estimate
-            if upnet:
-                flow_up = self.upnet(h, flow)
-            else:
-                flow_up = 8 * F.interpolate(flow, img1.shape[2:], mode='bilinear', align_corners=True)
-
-            out.append(flow_up)
-
-        return out
 
 
 class Raft(Model):
@@ -129,10 +38,10 @@ class Raft(Model):
         self.encoder_norm = encoder_norm
         self.context_norm = context_norm
 
-        super().__init__(RaftModule(dropout=dropout, mixed_precision=mixed_precision,
-                                    corr_radius=corr_radius, corr_channels=corr_channels,
-                                    context_channels=context_channels, recurrent_channels=recurrent_channels,
-                                    encoder_norm=encoder_norm, context_norm=context_norm), arguments)
+        super().__init__(raft.RaftModule(dropout=dropout, mixed_precision=mixed_precision,
+                                         corr_levels=1, corr_radius=corr_radius, corr_channels=corr_channels,
+                                         context_channels=context_channels, recurrent_channels=recurrent_channels,
+                                         encoder_norm=encoder_norm, context_norm=context_norm), arguments)
 
         self.adapter = raft.RaftAdapter()
 
