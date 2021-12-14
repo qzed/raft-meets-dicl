99a6a67ec136fb8a41da37d9f8db3e16dbc61aa1
Maximilian Luz <luzmaximilian@gmail.com>
2021-12-14 19:27:20 +0100

    Allow selecting groups for parameter/gradient norm metrics

--- src/metrics/grad_norm.py
+++ src/metrics/grad_norm.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Union
 
 import torch
+import torch.nn as nn
 
 from .common import Metric
 
@@ -32,7 +33,7 @@ class GradientNorm(Metric):
     def __init__(self, key: str = 'GradientNorm/', ord: float = 2, params: Union[str, List[str]] = 'total'):
         super().__init__()
 
-        if not isinstance(params, list) and params != 'all':
+        if not isinstance(params, (list, dict)) and params != 'all':
             params = [params]
 
         self.key = key
@@ -48,10 +49,20 @@ class GradientNorm(Metric):
 
     @torch.no_grad()
     def compute(self, model, optimizer, estimate, target, valid, loss):
+        def _collect(norms, pfx, ord):
+            ns = [v for k, v in norms.items() for p in pfx if k.startswith(p)]
+            return torch.tensor(ns).norm(p=ord).item()
+
         norms = grad_norm(model, self.ord)
+
+        # strip 'module.' prefix
+        if isinstance(model, (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel)):
+            norms = {k.removeprefix('module.'): v for k, v, in norms.items()}
 
         if self.params == 'all':
             norms = {f"{self.key}{name}": value for name, value in norms.items()}
+        elif isinstance(self.params, dict):
+            norms = {f"{self.key}{k}": _collect(norms, pfx, self.ord) for k, pfx in self.params.items()}
         else:
             norms = {f"{self.key}{name}": norms[name] for name in self.params}
 
