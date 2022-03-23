427189bae23d47d4a23a4c2502ea38668c25e74b
Maximilian Luz <luzmaximilian@gmail.com>
2022-03-23 01:21:10 +0100

    Fix DICL 1x1 matching network

--- src/models/common/corr/dicl_1x1.py
+++ src/models/common/corr/dicl_1x1.py
@@ -15,7 +15,7 @@ class MatchingNet1x1(nn.Sequential):
 
         super().__init__(
             ConvBlock(input_channels, c1, kernel_size=1, padding=1, norm_type=norm_type, relu_inplace=relu_inplace),
-            ConvBlock(c2, c2, kernel_size=1, padding=1, norm_type=norm_type, relu_inplace=relu_inplace),
+            ConvBlock(c1, c2, kernel_size=1, padding=1, norm_type=norm_type, relu_inplace=relu_inplace),
             ConvBlock(c2, c3, kernel_size=1, padding=1, norm_type=norm_type, relu_inplace=relu_inplace),
             nn.Conv2d(c3, 1, kernel_size=1, padding=1),     # note: with bias
         )
