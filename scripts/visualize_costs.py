b2983f01ca059d17a13a4e3c1f748ecd1c1c7946
Maximilian Luz <luzmaximilian@gmail.com>
2022-04-08 05:01:28 +0200

    scripts/visualize_costs: Add support for single-level RAFT

--- scripts/visualize_costs.py
+++ scripts/visualize_costs.py
@@ -134,6 +134,11 @@ def setup_hooks(model, activations):
         register_activation_hook_raft(model, activations, 'module.update_block')
         return
 
+    elif model.type == 'raft/sl':
+        register_reset_hook(model, activations)
+        register_activation_hook_raft(model, activations, 'module.update_block')
+        return
+
     elif model.type == 'raft/baseline':
         register_reset_hook(model, activations)
         register_activation_hook_raft(model, activations, 'module.update_block')
